c90459ab5263a2f37d4b8f23955990df25cb8185
archana-ravi <archana.ravi488@gmail.com>
2016-02-29 11:40:16 -0800

    - List name stays the same as entered by the user. (previously removed spaces from list name). Replaces spaces by underscore before insertion to DB and the other way around when you retrieve a list from DB to build the list object.

--- app/src/main/java/com/psu/acc/list_reminder/MainActivity.java
+++ app/src/main/java/com/psu/acc/list_reminder/MainActivity.java
@@ -67,18 +67,27 @@ public class MainActivity extends AppCompatActivity {
         databaseHelper.dropListsTable();
 
         //Create lists_table and three new lists.
-        // onCreate method to be called only if you hadn't dropped the lists table (like above)
+        // onCreate method to be called only if you hadn't dropped the lists table (like line 67)
         databaseHelper.onCreate(db);
         HashMap<String, String> items = new HashMap<>();
         items.put("Egg", "false");
         items.put("Milk", "false");
-        ListObject listObject1 = new ListObject("List 1", items, "Jan 11, 2016 5:00 am", "weekly", "true");
-        databaseHelper.addList(listObject1);
         items.put("Bread", "false");
-        ListObject listObject2 = new ListObject("List 2", items, "Jan 12, 2016 6:00 am", "monthly", "true");
-        databaseHelper.addList(listObject2);
         items.put("Banana", "false");
-        ListObject listObject3 = new ListObject("List 3", items, "Jan 13, 2016 7:00 am", "hourly", "true");
+        ListObject listObject1 = new ListObject("Grocery List", items, "March 12, 2016 10:00 am", "weekly", "true");
+        databaseHelper.addList(listObject1);
+        items = new HashMap<>();
+        items.put("Chairs", "false");
+        items.put("Dining table", "false");
+        items.put("Mattress", "false");
+        items.put("Coffee table", "false");
+        ListObject listObject2 = new ListObject("Garage Sale", items, "March 20, 2016 7:00 pm", "none", "true");
+        databaseHelper.addList(listObject2);
+        items = new HashMap<>();
+        items.put("Tees", "false");
+        items.put("Boots", "false");
+        items.put("Skateboard", "false");
+        ListObject listObject3 = new ListObject("Thanksgiving Shopping", items, "Nov 15, 2016 5:00 pm", "none", "true");
         databaseHelper.addList(listObject3);
 
         //Display newly created list names
@@ -88,40 +97,34 @@ public class MainActivity extends AppCompatActivity {
             System.out.println("\t" + listName);
         }
 
-        //Retrieve a list
-        System.out.println("Displaying List2");
-        ListObject aList = databaseHelper.displayList("List2");
+        //Retrieve a list and display its attributes.
+        System.out.println("Displaying Garage Sale List");
+        ListObject aList = databaseHelper.getList("Garage Sale");
         aList.displayList();
-        //Removes item Egg from list and display it
-        System.out.println("Remove item Egg from List2");
-        databaseHelper.removeItemFromList("List2", "Egg");
-        aList = databaseHelper.displayList("List2");
-        System.out.println("Updated List2");
+        //Removes item Dining table from list and display it
+        System.out.println("Remove item Dining table from Garage Sale");
+        databaseHelper.removeItemFromList("Garage Sale", "Dining table");
+        aList = databaseHelper.getList("Garage Sale");
+        System.out.println("Updated Garage Sale List");
         aList.displayList();
-        //Remove list1
-        System.out.println("Remove List1");
-        databaseHelper.removeList("List1");
+        //Remove Grocery List
+        System.out.println("Remove Grocery List");
+        databaseHelper.removeList("Grocery List");
 
-        //Display existing lists
-        existingLists = databaseHelper.getAllListNames();
-        System.out.println("Lists so far:");
-        for (String listName : existingLists){
-            System.out.println("\t" + listName);
-        }
-
-        //Update List3, strike off bread (change value from false to true), and add new item 'Nutella'
-        aList = databaseHelper.displayList("List3");
-        System.out.println("Original List3");
+        //Update Thanksgiving Shopping, strike off boots (change value from false to true), and add new item 'Home theatre'
+        aList = databaseHelper.getList("Thanksgiving Shopping");
+        System.out.println("Original Thanksgiving Shopping List");
         aList.displayList();
         Map<String, String> itemsList3 = aList.getListItems();
-        itemsList3.put("Nutella", "false");
-        itemsList3.put("Milk", "true");
+        itemsList3.put("Home theatre", "false");
+        itemsList3.put("Boots", "true");
         aList.setListItems(itemsList3);
         //Update list in the database, removes old one and creates a new one with the same name.
         databaseHelper.updateList(aList);
-        System.out.println("Updated List3");
-        aList = databaseHelper.displayList("List3");
+        System.out.println("Updated Thanksgiving Shopping");
+        aList = databaseHelper.getList("Thanksgiving Shopping");
         aList.displayList();
+
         displayExistingLists();
     }
 
