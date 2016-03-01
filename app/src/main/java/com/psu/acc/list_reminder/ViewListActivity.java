9b5a4922e2a26f2ffa4edecf0422df0cc5ea7c4d
Casey Bowman <caseybowman@bpccm.com>
2016-02-29 23:42:40 -0800

    [AndroidManifest] - Changed view_list layout to different theme. [ItemAdapter] - Adds list_item elements to view_list layout. [ViewListActivity] - Added support for ItemAdapter. [drawable][cancel] - Changed icon to be a bit more red. [layout][list_item] - Added id name to RelativeLayout. [layout[view_list] - Removed title edit button. [values][colors] - Added gray color. [values][strings] - Changed default_list_name string.

--- app/src/main/java/com/psu/acc/list_reminder/ViewListActivity.java
+++ app/src/main/java/com/psu/acc/list_reminder/ViewListActivity.java
@@ -1,9 +1,11 @@
 package com.psu.acc.list_reminder;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -14,16 +16,17 @@ import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 /**
  * Created by caseybowman on 2/13/16.
  */
-public class ViewListActivity extends AppCompatActivity {
+public class ViewListActivity extends Activity {
 
 
     private Button doneButton;
     private ListView itemList;
     private ArrayAdapter<LinearLayout> listAdapter;
-    private ImageButton editTitleButton;
     private ImageButton editReminderButton;
     private ImageButton addItemConfirmationButton;
     private CheckBox enableReminderCheckBox;
@@ -41,7 +44,6 @@ public class ViewListActivity extends AppCompatActivity {
 
         doneButton             = (Button) findViewById(R.id.done_button);
         itemList               = (ListView) findViewById(R.id.item_list);
-        editTitleButton        = (ImageButton) findViewById(R.id.edit_title_button);
         editReminderButton     = (ImageButton) findViewById(R.id.edit_reminder_button);
         addItemConfirmationButton = (ImageButton) findViewById(R.id.add_item_confirmation_button);
         enableReminderCheckBox = (CheckBox) findViewById(R.id.enable_reminder_checkbox);
@@ -49,18 +51,24 @@ public class ViewListActivity extends AppCompatActivity {
         addItemEditText        = (EditText) findViewById(R.id.add_item_edittext);
         reminderTextView       = (TextView) findViewById(R.id.reminder_textview);
 
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("data1");
+        list.add("data2");
+
+        itemList.setAdapter(new ItemAdapter(this, itemList, list));
 
         if (xtra!=null)
         {
             reminderTextView.setText("Reminder set to \n"+xtra.getString("date")+" "+xtra.getString("time"));
         }
 
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+
         doneButton.setOnClickListener(new View.OnClickListener() {
             // enable/disable visibility on views depending on what mode the user is in
             @Override
             public void onClick(View view) {
                 if (editMode == true) {
-                    editTitleButton.setVisibility(View.INVISIBLE);
                     editReminderButton.setVisibility(View.INVISIBLE);
                     addItemConfirmationButton.setVisibility(View.INVISIBLE);
                     enableReminderCheckBox.setVisibility(View.INVISIBLE);
@@ -72,7 +80,6 @@ public class ViewListActivity extends AppCompatActivity {
                     }
                     editMode = false;
                 } else {
-                    editTitleButton.setVisibility(View.VISIBLE);
                     editReminderButton.setVisibility(View.VISIBLE);
                     addItemConfirmationButton.setVisibility(View.VISIBLE);
                     enableReminderCheckBox.setVisibility(View.VISIBLE);
